eda5857ba0a9b932ccc00aac581b7a0bd12f9de8
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-02-20 16:53:03 -0800

    went overboard with appendListItem assignment. refactored to utilize jquery

---
+++ appendListItem/javascriptWithJQuery.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+$('button').on('click', function(event){
+		event.preventDefault();
+		// add user input from text field to a new list item with the class 'listItem' in the unordered list
+		$('#listOfItems').append($('<li>',
+		{text: document.getElementById('itemInput').value,
+		}));
+		$('li').addClass('listItem');
+		// clear the text input field after the button has been clicked
+		$('input:text').val('');
+    });
+});
+
