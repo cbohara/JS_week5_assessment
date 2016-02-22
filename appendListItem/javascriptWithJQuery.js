fa4142d524ff539a03f7512e001369841fbbce63
Charlie O'Hara <charlie.b.ohara@gmail.com>
2016-02-21 16:55:25 -0800

    minor edit

--- appendListItem/javascriptWithJQuery.js
+++ appendListItem/javascriptWithJQuery.js
@@ -4,9 +4,8 @@ $('button').on('click', function(event){
 		event.preventDefault();
 		// add user input from text field to a new list item with the class 'listItem' in the unordered list
 		$('#listOfItems').append($('<li>',
-		{text: document.getElementById('itemInput').value,
-		}));
-		$('li').addClass('listItem');
+			{text: document.getElementById('itemInput').value}));
+		$('li').(addClass('listItem');)
 		// clear the text input field after the button has been clicked
 		$('input:text').val('');
     });
